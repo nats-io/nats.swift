ae9681b851707077d2e2ac6988fe3ebbb0fb107e
Tomasz Pietrek <tomasz@nats.io>
2023-12-18 07:07:54 +0100

    Cleanup old tests
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Tests/NatsSwiftTests/Integration/ConnectionTests.swift
+++ Tests/NatsSwiftTests/Integration/ConnectionTests.swift
@@ -9,25 +9,32 @@ import Logging
 @testable import NatsSwift
 
 class ConnectionTests: XCTestCase {
-    
+
     static var allTests = [
         ("testNewClient", testNewClient)
     ]
-    
+   var natsServer = NatsServer()
+
+     override func tearDown() {
+         super.tearDown()
+         natsServer.stop()
+     }
+
     func testNewClient() async throws {
+        natsServer.start()
         logger.logLevel = .debug
         print("Testing new client")
         logger.debug("Testing new client with log")
-        let client = Client(url: URL(string: TestSettings.natsUrl)!)
+        let client = Client(url: URL(string: natsServer.clientURL)!)
         try await client.connect()
         let sub = try await client.subscribe(to: "test")
-        try await client.publish("msg", subject: "test")
-        
+        try client.publish("msg", subject: "test")
+
         if let msg = await sub.next() {
             print("Received on \(msg.subject): \(msg.payload!)")
         }
         await sub.complete()
-        
+
         XCTAssertNotNil(client, "Client should not be nil")
     }
 }
