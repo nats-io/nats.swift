80962f2b6beaa741e9c03e0478ce2be1b4ae5ba9
Tomasz Pietrek <tomasz@nats.io>
2023-12-28 14:48:28 +0100

    Improve Core NATS tests
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Tests/NatsSwiftTests/Integration/ConnectionTests.swift
+++ Tests/NatsSwiftTests/Integration/ConnectionTests.swift
@@ -8,10 +8,12 @@ import NIO
 import Logging
 @testable import NatsSwift
 
-class ConnectionTests: XCTestCase {
+class CoreNatsTests: XCTestCase {
 
     static var allTests = [
-        ("testNewClient", testNewClient)
+        ("testPublish", testPublish),
+        ("testPublishWithReply", testPublishWithReply),
+        ("testConnect", testConnect)
     ]
    var natsServer = NatsServer()
 
@@ -20,21 +22,50 @@ class ConnectionTests: XCTestCase {
          natsServer.stop()
      }
 
-    func testNewClient() async throws {
+     func testPublish() async throws {
         natsServer.start()
         logger.logLevel = .debug
-        print("Testing new client")
-        logger.debug("Testing new client with log")
         let client = Client(url: URL(string: natsServer.clientURL)!)
         try await client.connect()
         let sub = try await client.subscribe(to: "test")
+
         try client.publish("msg".data(using: .utf8)!, subject: "test")
-
-        if let msg = await sub.next() {
-            print("Received on \(msg.subject): \(msg.payload!.toString()!)")
+        let expectation = XCTestExpectation(description: "Should receive message in 5 seconsd")
+        Task {
+            if let msg = await sub.next() {
+                XCTAssertEqual(msg.subject, "test")
+                expectation.fulfill()
+            }
         }
+        wait(for: [expectation], timeout: 5.0)
         await sub.complete()
+     }
 
+     func testPublishWithReply() async throws {
+        natsServer.start()
+        logger.logLevel = .debug
+        let client = Client(url: URL(string: natsServer.clientURL)!)
+        try await client.connect()
+        let sub = try await client.subscribe(to: "test")
+
+        try client.publish("msg".data(using: .utf8)!, subject: "test", reply: "reply")
+        let expectation = XCTestExpectation(description: "Should receive message in 5 seconsd")
+        Task {
+            if let msg = await sub.next() {
+                XCTAssertEqual(msg.subject, "test")
+                XCTAssertEqual(msg.replySubject, "reply")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 5.0)
+        await sub.complete()
+     }
+
+    func testConnect() async throws {
+        natsServer.start()
+        logger.logLevel = .debug
+        let client = Client(url: URL(string: natsServer.clientURL)!)
+        try await client.connect()
         XCTAssertNotNil(client, "Client should not be nil")
     }
 }
