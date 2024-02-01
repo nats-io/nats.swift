4f96d21041f5ad70d2d3164aff9e8a53a654058e
Tomasz Pietrek <tomasz@nats.io>
2024-02-01 12:02:40 +0100

    Switch to stable release of nkeys.swift
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
-        .package(url: "https://github.com/nats-io/nkeys.swift.git", branch: "main" )
+        .package(url: "https://github.com/nats-io/nkeys.swift.git", from: "0.1.1")
     ],
     targets: [
         .target(name: "NatsSwift", dependencies: [
