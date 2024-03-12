b555f87432b34be36a8d5f5b2aee3c4ac437be85
Tomasz Pietrek <tomasz@nats.io>
2024-03-12 09:29:26 +0100

    Run swift-format
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Sources/NatsSwift/RttCommand.swift
+++ Sources/NatsSwift/RttCommand.swift
@@ -3,21 +3,21 @@ import NIOCore
 internal class RttCommand {
     let startTime = ContinuousClock().now
     let promise: EventLoopPromise<Duration>?
-    
+
     static func makeFrom(channel: Channel?) -> RttCommand {
         RttCommand(promise: channel?.eventLoop.makePromise(of: Duration.self))
     }
-    
+
     private init(promise: EventLoopPromise<Duration>?) {
         self.promise = promise
     }
-    
+
     func setRoundTripTime() {
         let now: ContinuousClock.Instant = ContinuousClock().now
         let rtt: Duration = now - startTime
         promise?.succeed(rtt)
     }
-    
+
     func getRoundTripTime() async throws -> Duration {
         try await promise?.futureResult.get() ?? Duration.zero
     }
