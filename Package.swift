744fee5dcb4e896c88869388050a3b0dfbad8ecf
Tomasz Pietrek <tomasz@nats.io>
2024-03-13 11:40:47 +0100

    Use Nuid v0.2
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
         .package(url: "https://github.com/nats-io/nkeys.swift.git", from: "0.1.1"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
-        .package(url: "https://github.com/Jarema/swift-nuid.git", from: "0.1.1"),
+        .package(url: "https://github.com/Jarema/swift-nuid.git", from: "0.2.0"),
     ],
     targets: [
         .target(
